009ca791297a3e2fd8abaa932251e03bcf208129
Kuba Wysocki <jestemjakubwysocki@gmail.com>
2019-09-14 13:24:15 +0200

    startAnimation optymalization

--- scripts/myscripts/load.js
+++ scripts/myscripts/load.js
@@ -1,18 +1,20 @@
 const load=()=>{
-    $('body').removeClass('stop_scrolling');
-    $('.loading').css('opacity', '0');
-    setTimeout(()=>$('.loading').css('display', 'none'),200);
+    $('body').removeClass('stop_scrolling')
+    $('.loading').css('opacity', '0')
+    setTimeout(()=>$('.loading').css('display', 'none'),200)
 }
 const startAnimation=()=>{
-    $(window).on('scroll',()=>{
-        let top=$(window).scrollTop();
-            $('.name').css('left', top/1.2);
-            $('.desc').css('left', -top*1.2);
-            $('.wmore').css('left', -top/2);
-            $('.btnStart').css('left', top/1.5);
-    });
-    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;   //iOS background fixed not working bug fix
-    if(!iOS) $('.preview').css('background-attachment','fixed');
+    $(window).on('scroll',() => {
+        let scrollTop=$(window).scrollTop()
+        if( scrollTop < $(window).height()  ){
+            $('.name').css({"-webkit-transform": `translateX(${ scrollTop / 1.2 }px)`})
+            $('.desc').css({"-webkit-transform": `translateX(${ -scrollTop * 1.2 }px)`})
+            $('.wmore').css({"-webkit-transform": `translateX(${ -scrollTop / 2 }px)`})
+            $('.btnStart').css({"-webkit-transform": `translateX(${ scrollTop / 1.5 }px)`})
+        }
+    })
+    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream //iOS background fixed not working bug fix
+    if(!iOS) $('.preview').css('background-attachment','fixed')
 }
 const swap=()=>{                        //this function changes order of ul and img on larger devices (md)
     if($(window).width()>=768){
